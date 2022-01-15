039e6f9575abed6908db38f68ca2939c61af76f1
muusah <92143912+muusah@users.noreply.github.com>
2022-01-15 16:02:11 +0700

    musa4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,14 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    // public function index(Request $request)
+    // {
+    //     $todo = Todo::where('user_id', $request->session()->get('user_id'));
+
+    //     return view('index', ["todos" => $todo]);
+    // }
+
     public function index()
     {
         return view('home');
